1be6da4f9d8f0545a5c55d56492639621665b828
Захар <zaharchertuhin@gmail.com>
2021-05-02 03:22:36 +0400

    homework 1.05.2021

--- revies/forms.py
+++ revies/forms.py
@@ -3,3 +3,7 @@ class Revies_form(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     review = forms.CharField(widget=forms.Textarea)
+
+class Show_form(forms.Form):
+    # sh_review = forms.CharField(widget=forms.Textarea)
+    rating = forms.IntegerField(max_value=100, min_value=5)
--- revies/views.py
+++ revies/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
-from revies.forms import Revies_form
+from revies.forms import Revies_form, Show_form
 # Create your views here.
 def Revies(request):
     if request.method == "GET":
         form = Revies_form()
-        return render(request, "index.html", {"form":form})
+        showform = Show_form()
+        return render(request, "index.html", {"form":form}, {"showform":showform})
     elif request.method == "POST":
         form = Revies_form(request.POST)
+        # showform = Show_form(request.POST)
         if form.is_valid():
             data = form.cleaned_data
             name = data.get("name")
@@ -14,7 +16,8 @@ def Revies(request):
             review = data.get("review")
             with open("../data.csv", "a") as file:
                 file.write(f"{name}|{email}|{review}\n")
-            return redirect("")
+            return redirect(Revies)
         else:
             form = Revies_form()
+            # showform = Show_form()
             return render(request, "index.html", {"form": form})
