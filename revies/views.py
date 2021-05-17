98db4d2dbf62e399bcab89b81fde38a8103ebafe
Захар <zaharchertuhin@gmail.com>
2021-05-17 19:26:31 +0400

    homework 17/05 second commit

--- revies/views.py
+++ revies/views.py
@@ -19,29 +19,13 @@ def Revies(request):
     with open("./data.csv") as file:
         revies = file.readlines()
     if len(revies) > 0:
-        revies_1 = revies[0]
+        revies_1 = revies[-1]
         name_1 = revies_1.split("|")[0]
-        email_1 = revies_1.split("|")[0]
-        review_1 = revies_1.split("|")[0]
+        email_1 = revies_1.split("|")[1]
+        review_1 = revies_1.split("|")[2]
     else:
         name_1 = ""
         email_1 = ""
         review_1 = ""
     form = Revies_form()
     return render(request, "index.html", {"form":form, "name":name_1, "email":email_1, "review":review_1})
-
-# def Showrev(request):
-#     if request.method == "GET":
-#         form = sh_form
-#         return render(request, "index.html", {"form": form})
-#     elif request.method == "POST":
-#         form = sh_form(request.POST)
-#         if form.is_valid():
-#             data = form.cleaned_data
-#             rating = data.get("rating")
-#             with open("../data.csv", "a") as file:
-#                 file.write(f"{rating}\n")
-#             return redirect(Showrev)
-#         else:
-#             form = sh_form()
-#             return render(request, "index.html", {"form": form})
